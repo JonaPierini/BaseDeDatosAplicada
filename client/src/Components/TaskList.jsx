372679129d4105b1941cab4c8683633f5843f21b
Jona Pierini <jonapierini@gmail.com>
2022-10-22 11:46:26 -0300

    server-cliente terminado

--- client/src/Components/TaskList.jsx
+++ client/src/Components/TaskList.jsx
@@ -1,37 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const TaskList = () => {
-  const [task, setTask] = useState("");
+  const [taskInput, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   const onChange = (e) => {
     console.log(e.target.value);
-    setTask(e.target.value);
+    setInput(e.target.value);
   };
 
-  const onDeleteClick = () => {
-    console.log('delete')
+  const onDeleteClick = async (id) => {
+    const resultado = await fetch(`http://localhost:4000/deleteTasks/${id}`, {method:'DELETE'})
+    if(!resultado.ok){
+      console.log('fallo')
+      return
+    } 
+    setTasks(tasks.filter((elem)=> elem.id !== id))
   }
+
+  const onSubmitData = async (e) => {
+    e.preventDefault()
+    const resultado = await fetch(`http://localhost:4000/addTasks`, {method:'POST', body:JSON.stringify({task:taskInput})})
+    if(!resultado.ok){
+      console.log('fallo')
+      return
+    } 
+    const data = await resultado.json();
+    setTasks([...tasks, {name: taskInput, id: data.id}])
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:4000/tasks')
+    .then(response => response.json())
+    .then(data => setTasks(data));
+  }, [ ])
+  
+ 
   return (
     <div>
       <h3>TaskList</h3>
       <div className="ui input">
-        <input placeholder="your task" value={task} onChange={onChange}></input>
-        <button className="ui primary button basic">Submit</button>
+        <input placeholder="your task" value={taskInput} onChange={onChange}></input>
+        <button className="ui primary button basic" onClick={onSubmitData}>Submit</button>
       </div>
       <hr></hr>
-      <div className="ui cards">
+    { tasks.map((elem) => (
+      <div key={elem.id} className="ui cards">
         <div className="card">
           <div className="content">
-            <div className="meta">Friends of Vero</div>
+            <div className="meta">{elem.name}</div>
           </div>
           <div className="extra content">
             <div className="ui two buttons">
               <div className="ui basic green button">Done</div>
-              <div className="ui basic red button" onClick={onDeleteClick}>Delete</div>
+              <div className="ui basic red button" onClick={()=> onDeleteClick(elem.id)}>Delete</div>
             </div>
           </div>
         </div>
       </div>
+    )) }
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,24 +1,44 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser');
 
 
 const app = express()
 
 app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+let taskList = [{
+    name: 'tarea1',
+    id: new Date().getMilliseconds()
+},{ 
+    name: 'tarea2',
+    id: new Date().getMilliseconds()
+},
+];
+
 app.get('/tasks', (req, res)=> {
-    res.send('list of all task')
+    res.status(200).send(taskList)
 })
 
-app.get('/addTasks', (req, res)=> {
-    res.send('add task')
+app.post('/addTasks', (req, res)=> {
+    const id = new Date().getMilliseconds()
+    taskList.push({
+        name:req.body.task,
+        id: id
+    })
+    res.status(200).send({
+        id
+    })
 })
 
-app.get('/deleteTasks', (req, res)=> {
-    res.send('delete')
+app.delete('/deleteTasks/:id', (req, res)=> {
+    taskList = taskList.filter((elem)=> elem.id !== req.params.id)
+    res.status(200).send()
 })
-
-
 
 app.listen(4000, ()=> {
     console.log('running on port 4000')
